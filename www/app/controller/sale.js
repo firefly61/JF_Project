f448f864c2ee454cbdb69a3f2041ef2e7144575f
firefly61 <nicoledeyx@163.com>
2020-03-13 18:00:29 +0800

    添加销售

---
+++ www/app/controller/sale.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const BaseController = require('./base')
+
+class SaleController extends BaseController {
+
+    async create () {
+        const {ctx} = this
+    }
+
+}
+
+module.exports = SaleController 
